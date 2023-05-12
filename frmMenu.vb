abbfc5564f16320ede48abb9dbb49f0ccd700060
Vinicius Nunes Carvalho <vinicarvalho0609@gmail.com>
2023-05-12 11:56:12 -0300

    feat: add exit  e msgbox de confirmação no botão sair

--- frmMenu.vb
+++ frmMenu.vb
@@ -1,3 +1,8 @@
 ﻿Public Class frmMenu
-
+    Private Sub btn_sair_Click(sender As Object, e As EventArgs) Handles btn_sair.Click
+        Dim resp = MsgBox("Deseja realmente sair?", MsgBoxStyle.YesNo, "Selecione uma resposta!")
+        If resp = vbYes Then
+            Application.Exit()
+        End If
+    End Sub
 End Class
