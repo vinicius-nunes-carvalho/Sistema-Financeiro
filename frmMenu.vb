c1667b716c132b91548949588b6d488bb8f5f177
Vinicius Nunes Carvalho <vinicarvalho0609@gmail.com>
2023-05-12 17:51:02 -0300

    feat: o btn gravar irá validar se todos campos estão completos

--- frmMenu.vb
+++ frmMenu.vb
@@ -1,8 +1,28 @@
 ﻿Public Class frmMenu
+
+    Public Sub validar()
+        Do While cmb_categoria.SelectedIndex = -1 Or
+                 cmb_pagamento.SelectedIndex = -1 Or
+                 txt_compra.Text = "" Or
+                 txt_valor.Text = "" Or
+                 Not dtp_data.Checked
+            MsgBox("Complete todos os campos para realizar alguma ação", MessageBoxIcon.Warning, "Aviso!")
+            Exit Do
+        Loop
+    End Sub
     Private Sub btn_sair_Click(sender As Object, e As EventArgs) Handles btn_sair.Click
         Dim resp = MsgBox("Deseja realmente sair?", MsgBoxStyle.YesNo, "Selecione uma resposta!")
         If resp = vbYes Then
             Application.Exit()
         End If
+    End Sub
+
+    Private Sub frmMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmb_categoria.Items.Add("teste")
+        cmb_pagamento.Items.Add("teste")
+    End Sub
+
+    Private Sub btn_gravar_Click(sender As Object, e As EventArgs) Handles btn_gravar.Click
+        validar()
     End Sub
 End Class
